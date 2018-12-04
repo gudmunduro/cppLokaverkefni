5205f661e38cb395036be80173825b5b75070b5a
Guðmundur <gudmunduro>
2018-12-04 20:16:58 +0000

    List

--- Source/List.cpp
+++ Source/List.cpp
@@ -1,5 +1,43 @@
 #include "List.h"
 
-LinkedList::LinkedList() {
+PersonList::PersonList() {
+
+}
+
+void PersonList::add() {
+
+}
+
+int PersonList::count() {
+
+}
+
+PersonNode* PersonList::get(int id) {
+
+}
+
+void PersonList::remove() {
+
+}
+
+// Flight
+
+FlightList::FlightList() {
+
+}
+
+void FlightList::add() {
+
+}
+
+int FlightList::count() {
+
+}
+
+FlightNode* FlightList::get(int id) {
+
+}
+
+void FlightList::remove() {
 
 }
--- Source/List.h
+++ Source/List.h
@@ -1,3 +1,35 @@
 #include "includes.h"
+#include "Person.h"
+#include "FlightBooking.h"
 
+struct PersonNode {
+    Person *person;
+    PersonNode *next;
+};
 
+class PersonList {
+private:
+    PersonNode *first;
+public:
+    PersonList();
+    void add();
+    void remove();
+    int count();
+    PersonNode *get(int id);
+};
+
+struct FlightNode {
+    FlightBooking *flight;
+    FlightNode *next;
+};
+
+class FlightList {
+private:
+    FlightNode *first;
+public:
+    FlightList();
+    void add();
+    void remove();
+    int count();
+    FlightNode *get(int id);
+};
